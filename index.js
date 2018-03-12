441759d04a0edab74f20bca1e138aaba0a684239
Derek Miranda <derek.miranda@ff0000.com>
2018-03-12 12:31:59 -0700

    Just use regular context for url-loader - don't need to set any default settings for desired inlining output

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ module.exports = function(source) {
   if (this.resourceQuery) {
     const queryObj = qs.parse(this.resourceQuery.slice(1))
     if (queryObj.inline) {
-      const urlLoaderCtx = Object.assign({
-        // default settings for inlining images
-        emitFile: false,
-        limit: Infinity,
-      }, this)
-      return require('url-loader').call(urlLoaderCtx, source)
+      return require('url-loader').call(this, source)
     }
   }
   var parsedPath = path.parse(this.resourcePath)
