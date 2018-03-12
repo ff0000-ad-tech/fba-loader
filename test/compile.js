74e62270610d63e5e374e0992483b864c16be58e
Derek Miranda <derek.miranda@ff0000.com>
2018-03-12 12:31:29 -0700

    Script for updating compiled test src files

---
+++ test/compile.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack')
+const path = require('path')
+
+function compile(entry, options) {
+	const entryPath = path.resolve(__dirname, 'src/fixtures', entry)
+	const compiler = webpack({
+		entry: entryPath,
+		bail: true,
+		output: {
+			path: path.resolve(__dirname, 'src/expected'),
+			filename: entry
+		},
+		module: {
+			rules: [{
+				test: /.jpg$/,
+				use: [{
+					loader: path.join(__dirname, '../index.js'),
+					options,
+				}]
+			}],
+		},
+	});
+	compiler.run((err, stats) => {
+		if (err) throw err
+	})
+}
+
+const inlineHiFile = path.resolve(__dirname, 'src/fixtures/hi.inline.js')
+
+compile('hi.js')
+compile('hi.inline.js')
