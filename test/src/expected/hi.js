d639297eb4868eb4288fddd24e778183161396e1
Jacques Altounian <cattlebane@gmail.com>
2020-08-11 17:39:16 -0700

    fix formatting of hi.js

--- test/src/expected/hi.js
+++ test/src/expected/hi.js
@@ -1,91 +1,84 @@
-/******/ ;(function(modules) {
-  // webpackBootstrap
-  /******/ // The module cache
-  /******/ var installedModules = {} // The require function
-  /******/
-  /******/ /******/ function __webpack_require__(moduleId) {
-    /******/
-    /******/ // Check if module is in cache
-    /******/ if (installedModules[moduleId]) {
-      /******/ return installedModules[moduleId].exports
-      /******/
-    } // Create a new module (and put it into the cache)
-    /******/ /******/ var module = (installedModules[moduleId] = {
-      /******/ i: moduleId,
-      /******/ l: false,
-      /******/ exports: {},
-      /******/
-    }) // Execute the module function
-    /******/
-    /******/ /******/ modules[moduleId].call(module.exports, module, module.exports, __webpack_require__) // Flag the module as loaded
-    /******/
-    /******/ /******/ module.l = true // Return the exports of the module
-    /******/
-    /******/ /******/ return module.exports
-    /******/
-  } // expose the modules object (__webpack_modules__)
-  /******/
-  /******/
-  /******/ /******/ __webpack_require__.m = modules // expose the module cache
-  /******/
-  /******/ /******/ __webpack_require__.c = installedModules // define getter function for harmony exports
-  /******/
-  /******/ /******/ __webpack_require__.d = function(exports, name, getter) {
-    /******/ if (!__webpack_require__.o(exports, name)) {
-      /******/ Object.defineProperty(exports, name, {
-        /******/ configurable: false,
-        /******/ enumerable: true,
-        /******/ get: getter,
-        /******/
-      })
-      /******/
-    }
-    /******/
-  } // getDefaultExport function for compatibility with non-harmony modules
-  /******/
-  /******/ /******/ __webpack_require__.n = function(module) {
-    /******/ var getter =
-      module && module.__esModule
-        ? /******/ function getDefault() {
-            return module['default']
-          }
-        : /******/ function getModuleExports() {
-            return module
-          }
-    /******/ __webpack_require__.d(getter, 'a', getter)
-    /******/ return getter
-    /******/
-  } // Object.prototype.hasOwnProperty.call
-  /******/
-  /******/ /******/ __webpack_require__.o = function(object, property) {
-    return Object.prototype.hasOwnProperty.call(object, property)
-  } // __webpack_public_path__
-  /******/
-  /******/ /******/ __webpack_require__.p = '' // Load entry module and return exports
-  /******/
-  /******/ /******/ return __webpack_require__((__webpack_require__.s = 0))
-  /******/
-})(
-  /************************************************************************/
-  /******/ [
-    /* 0 */
-    /***/ function(module, __webpack_exports__, __webpack_require__) {
-      'use strict'
-      Object.defineProperty(__webpack_exports__, '__esModule', { value: true })
-      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__hi_jpg__ = __webpack_require__(1)
-      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__hi_jpg___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__hi_jpg__)
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-      /***/
-    },
+    "use strict";
+    Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__hi_jpg__ = __webpack_require__(1);
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__hi_jpg___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__hi_jpg__);
+    
+    
+    
+    /***/ }),
     /* 1 */
-    /***/ function(module, exports) {
-      // Test will fail unless you update this path to your local machine's path
-      throw new Error(
-        "Module build failed: TypeError: Cannot read property 'base64Inline' of null\n    at Object.module.exports (/Users/jacquesaltounian/Projects/ff0000-ad-tech/fba-loader/index.js:15:11)"
-      )
-
-      /***/
-    },
-    /******/
-  ]
-)
+    /***/ (function(module, exports) {
+    
+    throw new Error("Module build failed: TypeError: Cannot read property 'base64Inline' of null\n    at Object.module.exports (/Users/jacquesaltounian/Projects/ff0000-ad-tech/fba-loader/index.js:15:11)");
+    
+    /***/ })
+    /******/ ]);
